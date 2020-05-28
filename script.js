497bf57cd017439129b28b25032abd15c62c22b6
Sâm <dangvansam98@gmail.com>
2020-05-28 17:52:36 +0700

    xong

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ class Line {
         this.starty = starty
         this.endx = endx
         this.endy = endy
+        this.points = []
     }
 
     len(){
@@ -29,21 +30,124 @@ class Line {
         //console.log(mid_p)
         return mid_p
     }
+    setPoint(p){
+      this.points.push(p)
+    }
+    getPoint(){
+      return this.points
+    }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = [];
+    this.adjacencyList = {};
+  }
+  addNode(node) {
+    this.nodes.push(node); 
+    this.adjacencyList[node] = [];
+  }
+  addEdge(node1, node2, weight) {
+    this.adjacencyList[node1].push({node:node2, weight: weight});
+    this.adjacencyList[node2].push({node:node1, weight: weight});
+  }
+
+  findPathWithDijkstra(startNode, endNode) {
+
+    let times = {};
+    let backtrace = {};
+    let result = [];
+    if (this.adjacencyList[startNode].length == 0 || this.adjacencyList[endNode].length == 0){
+      result = ["INFINITY",[]];
+      return result;
+    }
+    let pq = new PriorityQueue();
+    times[startNode] = 0;
+    
+    this.nodes.forEach(node => {
+      if (node !== startNode) {
+        times[node] = Infinity
+      }
+    });
+    pq.enqueue([startNode, 0]);
+    while (!pq.isEmpty()) {
+      let shortestStep = pq.dequeue();
+      let currentNode = shortestStep[0];
+      this.adjacencyList[currentNode].forEach(neighbor => {
+        let time = times[currentNode] + neighbor.weight;
+        if (time < times[neighbor.node]) {
+          times[neighbor.node] = time;
+          backtrace[neighbor.node] = currentNode;
+          pq.enqueue([neighbor.node, time]);
+        }
+      });
+    }
+    let path = [endNode];
+    let lastStep = endNode;
+    while(lastStep !== startNode) {
+      path.unshift(backtrace[lastStep])
+      lastStep = backtrace[lastStep]
+    }
+    //path.push(times[endNode])
+    result.push(times[endNode])
+    result.push(path)
+    
+    //console.log("result:",result)
+    return result
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.collection = [];
+  }
+  enqueue(element){
+    if (this.isEmpty()){ 
+      this.collection.push(element);
+    } else {
+      let added = false;
+      for (let i = 1; i <= this.collection.length; i++){
+        if (element[1] < this.collection[i-1][1]){ 
+          this.collection.splice(i-1, 0, element);
+          added = true;
+          break;
+        }
+      }
+      if (!added){
+          this.collection.push(element);
+      }
+    }
+  };
+
+  dequeue() {
+    let value = this.collection.shift();
+    return value;
+  };
+  isEmpty() {
+    return (this.collection.length === 0) 
+  };
 }
 
 
 function drawGraph(){
-
+    // Khởi tạo canvas
     init()
 
-    var num_point = document.getElementById("num_point_input").value;
-    var num_line = document.getElementById("num_line_input").value;
+    num_point = document.getElementById("num_point_input").value;
+    num_line = document.getElementById("num_line_input").value;
     num_point = parseInt(num_point)
     num_line = parseInt(num_line)
     
-    console.log("create_graph:",num_point,num_line)
-    //document.addEventListener('DOMContentLoaded', init)
+    var start_point = document.getElementById("start_point_input");
+    var end_point = document.getElementById("end_point_input");
+    start_point.value = 0;
+    end_point.value = 3;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    destance_text = document.getElementById("destance");
+    path_text = document.getElementById("path");
+    destance_text.innerHTML = ""
+    path_text.innerHTML = ""
 
     window_x = 100
     window_y = 100
@@ -52,20 +156,23 @@ function drawGraph(){
 
     list_point  = []
     list_line = []
+    map = new Graph();
 
     for (var i=0; i<num_point; i++){
-        tmp_x = getRndInteger(window_x, window_w)
-        tmp_y = getRndInteger(window_y, window_h)
-        tmp_p = new Point(tmp_x, tmp_y)
+        var tmp_x = getRndInteger(window_x, window_w)
+        var tmp_y = getRndInteger(window_y, window_h)
+        var tmp_p = new Point(tmp_x, tmp_y)
  
-        //drawPoint(tmp_p, i)
+        map.addNode(i)
         list_point.push(tmp_p)
-        //console.log("list_point:",list_point)
-        
+
         if (i != 0){
             for (var j=0; j<list_point.length; j++){
-                p = list_point[j]
-                tmp_line = new Line(p.x, p.y, tmp_p.x, tmp_p.y)
+                var p = list_point[j]
+                var tmp_line = new Line(p.x, p.y, tmp_p.x, tmp_p.y)
+                tmp_line.setPoint(i)
+                tmp_line.setPoint(j)
+
                 if (tmp_line.len()!=0){
                     list_line.push(tmp_line)
                 }
@@ -73,26 +180,90 @@ function drawGraph(){
         }
 
     }
-    // console.log(list_line[0])
+    // trộn các cạnh ngẫu nhiên
     list_line = shuffle(list_line)
-    // console.log(list_line[0])
-    // console.log(list_point.length, list_line.length)
 
     if (num_line > list_line.length){
         num_line = list_line.length;
     }
     for (var i=0; i<num_line; i++){
-        //console.log('i:',i)
-        tmp_line = list_line[i]
+        var tmp_line = list_line[i]
+        nodes = tmp_line.getPoint().sort()
+        map.addEdge(nodes[0], nodes[1], tmp_line.len())
         drawLine(tmp_line)
     }
     for (var i=0; i<list_point.length; i++){
-        tmp_p = list_point[i]
-        drawPoint(tmp_p, i+1)
+        var tmp_p = list_point[i]
+        drawPoint(tmp_p, i)
     }
 
+    //rs = map.findPathWithDijkstra(0,3)
+    //console.log(rs)
 }
 
+function isInclude(arr, x){
+    for (var i=0;i<arr.length;i++){
+      if (arr[i]==x){
+        return true;
+      }
+    }
+    return false;
+}
+function drawGraphDijsktra(){
+    var start_point = document.getElementById("start_point_input").value;
+    var end_point = document.getElementById("end_point_input").value;
+    start_point = parseInt(start_point)
+    end_point = parseInt(end_point)
+    if (start_point >= num_point || end_point >= num_point){
+        if (start_point >=num_point){
+          start_point = num_point -1
+        }
+        if (end_point >=num_point){
+          end_point = num_point -1
+        }
+      }
+    rs = map.findPathWithDijkstra(start_point, end_point)
+
+    destance = rs[0]
+    path = rs[1]
+    //console.log(destance)
+    //console.log(path)
+
+    destance_text.innerHTML = destance;
+    path_text.innerHTML = path.join(">");
+
+    var path = Object.keys(path).map(function (key) { return path[key]; });
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (var i=0; i<num_line; i++){
+      var tmp_line = list_line[i]
+
+      if (isInclude(path,tmp_line.getPoint()[0]) && isInclude(path,tmp_line.getPoint()[1])){
+        drawLine(tmp_line, c="#FF6347")
+        // console.log("true")
+        // console.log(path)
+        // console.log(tmp_line.getPoint()[0])
+        // console.log(tmp_line.getPoint()[1])
+      }
+      else{
+        drawLine(tmp_line)
+      }
+    }
+    for (var i=0; i<list_point.length; i++){
+        var tmp_p = list_point[i]
+        if (path.indexOf(i) != -1){
+          drawPoint(tmp_p, i, c="#FF6347")
+        }
+        else {
+            drawPoint(tmp_p, i)
+        }
+    }
+
+
+
+  
+  
+}
 function drawPoint(p, t, c="#57ff7b") {
     ctx.fillStyle = c
     ctx.beginPath();
@@ -103,7 +274,7 @@ function drawPoint(p, t, c="#57ff7b") {
     ctx.fillText(t, p.x-4, p.y+4);
 }
 
-function drawLine(l, c="black"){
+function drawLine(l, c="grey"){
     ctx.strokeStyle = c;
     ctx.beginPath();
     ctx.moveTo(l.startx, l.starty);
@@ -131,87 +302,3 @@ function shuffle(array) {
   
     return array;
   }
-
-  function shortestPath(edges, numVertices, startVertex) {
-    var done = new Array(numVertices);
-    done[startVertex] = true;
-    var pathLengths = new Array(numVertices);
-    var predecessors = new Array(numVertices);
-    for (var i = 0; i < numVertices; i++) {
-      pathLengths[i] = edges[startVertex][i];
-      if (edges[startVertex][i] != Infinity) {
-        predecessors[i] = startVertex;
-      }
-    }
-    pathLengths[startVertex] = 0;
-    for (var i = 0; i < numVertices - 1; i++) {
-      var closest = -1;
-      var closestDistance = Infinity;
-      for (var j = 0; j < numVertices; j++) {
-        if (!done[j] && pathLengths[j] < closestDistance) {
-          closestDistance = pathLengths[j];
-          closest = j;
-        }
-      }
-      done[closest] = true;
-      for (var j = 0; j < numVertices; j++) {
-        if (!done[j]) {
-          var possiblyCloserDistance = pathLengths[closest] + edges[closest][j];
-          if (possiblyCloserDistance < pathLengths[j]) {
-            pathLengths[j] = possiblyCloserDistance;
-            predecessors[j] = closest;
-          }
-        }
-      }
-    }
-    return { "startVertex": startVertex,
-             "pathLengths": pathLengths,
-             "predecessors": predecessors };
-  }
-  
-  function constructPath(shortestPathInfo, endVertex) {
-    var path = [];
-    while (endVertex != shortestPathInfo.startVertex) {
-      path.unshift(endVertex);
-      endVertex = shortestPathInfo.predecessors[endVertex];
-    }
-    return path;
-  }
-
-function Create2DArray(rows) {
-    var arr = [];
-  
-    for (var i=0; i<rows; i++) {
-       arr[i] = [];
-    }
-  
-    return arr;
-  }
-
-function createMatrix(list_point, list_line){
-    var _ = Infinity;
-    
-
-}
-  // The adjacency matrix defining the graph.
-  // var _ = Infinity;
-//   var e = [
-//     [ _, _, _, _, _, _, _, _, 4, 2, 3 ],
-//     [ _, _, 5, 2, 2, _, _, _, _, _, _ ],
-//     [ _, 5, _, _, _, 1, 4, _, _, _, _ ],
-//     [ _, 2, _, _, 3, 6, _, 3, _, _, _ ],
-//     [ _, 2, _, 3, _, _, _, 4, 3, _, _ ],
-//     [ _, _, 1, 6, _, _, 2, 5, _, _, _ ],
-//     [ _, _, 4, _, _, 2, _, 5, _, _, 3 ],
-//     [ _, _, _, 3, 4, 5, 5, _, 3, 2, 4 ],
-//     [ 4, _, _, _, 3, _, _, 3, _, 3, _ ],
-//     [ 2, _, _, _, _, _, _, 2, 3, _, 3 ],
-//     [ 3, _, _, _, _, _, 3, 4, _, 3, _ ]
-//   ];
-  
-  // Compute the shortest paths from vertex number 1 to each other vertex
-  // in the graph.
-  // var shortestPathInfo = shortestPath(e, 11, 1);
-  
-  // Get the shortest path from vertex 1 to vertex 6.
-  // var path1to6 = constructPath(shortestPathInfo, 6);
